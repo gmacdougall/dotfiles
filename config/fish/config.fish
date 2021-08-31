ef5001b40f382ec6fbc53dc2694f54124ca46541
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-08-31 16:56:51 -0400

    Some simple changes

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,10 +3,12 @@ fish_vi_key_bindings
 
 abbr -a -U -- dc docker-compose
 abbr -a -U -- g git
+abbr -a -U -- gp git push -u origin HEAD
 
 set -U CHRUBY_ROOT $HOME/.local
 set -gx GPG_TTY (tty)
 set -gx FZF_DEFAULT_COMMAND "fd --type f"
+set -gx EDITOR vim
 
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
--- config/nvim/lua/gm/plugins/fzf/remap.lua
+++ config/nvim/lua/gm/plugins/fzf/remap.lua
@@ -1,3 +1,2 @@
 Mapper = require("nvim-mapper")
-Mapper.map('n', '<leader>e', ":GFiles<CR>", {silent = true, noremap = true}, "fzf", "fzf_git_files", "Show all git files")
-Mapper.map('n', '<leader>E', ":Files<CR>", {silent = true, noremap = true}, "fzf", "fzf_files", "Show git files")
+Mapper.map('n', '<leader>e', ":Files<CR>", {silent = true, noremap = true}, "fzf", "fzf_files", "Show all files")
