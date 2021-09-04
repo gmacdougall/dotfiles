ac91742bd5ba4649da5ab507df6c23f8397e0d2b
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-09-04 09:10:33 -0400

    Remove submodules
    These are going to be installed inside the VM instead

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,6 +5,10 @@ abbr -a -U -- dc docker-compose
 abbr -a -U -- ds devspace
 abbr -a -U -- g git
 abbr -a -U -- gp git push -u origin HEAD
+abbr -a -U -- v vagrant
+abbr -a -U -- vp vagrant provision
+abbr -a -U -- vs vagrant ssh
+abbr -a -U -- vu vagrant up
 abbr -a -U -- y yarn
 
 set -U CHRUBY_ROOT $HOME/.local
