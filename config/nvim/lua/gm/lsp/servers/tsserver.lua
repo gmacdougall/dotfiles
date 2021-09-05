db7478e3699dfb72a571c4d51e362d9800a6f471
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-09-04 22:24:44 -0400

    Use global tsserver

--- config/nvim/lua/gm/lsp/servers/tsserver.lua
+++ config/nvim/lua/gm/lsp/servers/tsserver.lua
@@ -1,5 +1,5 @@
 return function(language_server_path, on_attach)
-    local bin_path = language_server_path .. "/typescript-language-server/typescript-language-server"
+    local bin_path = "typescript-language-server"
     return {
         cmd = {bin_path, "--stdio" },
         on_attach = function(client, bufnr)
