e6afabd7d0072019cbac5ee9bcee45fb2998b036
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-09-05 23:18:47 -0400

    Only find root by sorbet
    We shouldn't consider a project to be a sorbet supporting Ruby project
    just because it has a Gemfile.

--- config/nvim/lua/gm/lsp/servers/sorbet.lua
+++ config/nvim/lua/gm/lsp/servers/sorbet.lua
@@ -2,5 +2,5 @@ local lsp = require('lspconfig')
 
 return {
   cmd = { "srb", "tc", "--lsp", "--disable-watchman" },
-  root_dir = lsp.util.root_pattern("Gemfile", "sorbet"),
+  root_dir = lsp.util.root_pattern("sorbet"),
 }
