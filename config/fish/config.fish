680c04fabdc31e2117d4be54f9e122c6e9d0612d
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-09-13 18:33:22 +0000

    Recent branch checkout

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,7 @@ fish_vi_key_bindings
 abbr -a -U -- dc docker-compose
 abbr -a -U -- ds devspace
 abbr -a -U -- g git
+abbr -a -U -- cr "git checkout (git recentb | fzf --ansi | cut -d ' ' -f1)"
 abbr -a -U -- gp git push -u origin HEAD
 abbr -a -U -- v vagrant
 abbr -a -U -- vp vagrant provision
