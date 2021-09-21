a6123651df4eead6e90f286e94723802742bfeff
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-09-21 17:20:27 +0000

    Switch to prettierd

--- config/nvim/lua/gm/lsp/servers/formatters/prettier_d.lua
+++ config/nvim/lua/gm/lsp/servers/formatters/prettier_d.lua
@@ -1,7 +1,6 @@
--- npm add --global prettier_d_slim
 local prettier  = {
-  formatCommand = "prettier_d_slim --stdin --stdin-filepath ${INPUT}",
-  formatStdin = true
+  formatCommand = "prettierd ${INPUT}",
+  formatStdin = true,
 }
 
 return prettier
