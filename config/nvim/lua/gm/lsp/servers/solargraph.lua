4fc1a29d3fc60fa5a4146975911fe13ff9cf4375
Gregor MacDougall <gmacdougall0+github@gmail.com>
2021-12-12 08:41:12 -0500

    Better solargraph config

--- config/nvim/lua/gm/lsp/servers/solargraph.lua
+++ config/nvim/lua/gm/lsp/servers/solargraph.lua
@@ -1,5 +1,9 @@
+local lsp = require('lspconfig')
+
 return {
+  cmd = { "solargraph", "stdio" },
   init_options = {
     formatting = false,
   },
+  root_dir = lsp.util.root_pattern(".solargraph.yml")
 }
